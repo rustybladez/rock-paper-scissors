26515b73ffb381d01b48a084481516ab857efa74
Zunaid <zpokean@gmail.com>
2022-02-27 12:29:39 +0600

    Make playerPlay

--- game.js
+++ game.js
@@ -1,3 +1,21 @@
+function playerPlay(){
+    const playerChoice = prompt("Rock, Paper or Scissors?")
+
+    if (playerChoice.toLowerCase = 'rock')
+        return "You: Rock"
+    else if (playerChoice.toLowerCase = 'paper')
+        return "You: Paper"
+    else if (playerChoice.toLowerCase = 'scissors')
+        return "You: Scissors"
+    else return "Pick one of the three"
+}
+console.log(playerPlay());
+
+
+
+
+
+
 function computerPlay(){
     const computerChoice = ['rock', 'paper', 'scissors']
     const returnChoice = computerChoice[Math.floor((Math.random() * computerChoice.length))];
@@ -6,3 +24,4 @@ function computerPlay(){
     
 }
 console.log(computerPlay());
+
